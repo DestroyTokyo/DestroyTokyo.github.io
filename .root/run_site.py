22b95570cfe7adbaab30932aa0fe1a3f0f6a7200
Nionim <delta.cion.fe@gmail.com>
2026-07-17 04:11:51 +0300

    added page

---
+++ .root/run_site.py
@@ -0,0 +1,18 @@
+import http.server
+import socketserver
+import subprocess
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.join(os.path.dirname(ROOT_DIR), "site")
+
+PORT = 1663
+Handler = http.server.SimpleHTTPRequestHandler
+
+os.chdir(BASE_DIR)
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    subprocess.run([sys.executable, os.path.join(ROOT_DIR, "generate_site_assets.py")])
+    print(f"Server started on: http://localhost:{PORT}")
+    httpd.serve_forever()
